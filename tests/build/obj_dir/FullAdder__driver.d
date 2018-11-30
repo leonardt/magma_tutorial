08f6199aa178d9756202fc6589f1944ce4577c0a
Lenny Truong <lenny@stanford.edu>
2018-11-29 20:57:57 -0800

    Fix gitignore file name

--- tests/build/obj_dir/FullAdder__driver.d
+++
@@ -1,4 +0,0 @@
-FullAdder__driver.o: ../FullAdder__driver.cpp VFullAdder_.h \
-  /usr/local/Cellar/verilator/HEAD-304a24d/share/verilator/include/verilated.h \
-  /usr/local/Cellar/verilator/HEAD-304a24d/share/verilator/include/verilatedos.h \
-  /usr/local/Cellar/verilator/HEAD-304a24d/share/verilator/include/verilated_vcd_c.h
